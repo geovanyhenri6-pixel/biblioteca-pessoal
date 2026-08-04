adf68d63e656cb4cc9dd9941613f8c7cc1009d26
Roku <geovanyhenri6@gmail.com>
2026-08-04 19:46:31 -0300

    sistema de autenticação completo: cadastro, login, logout, teplates

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
 from database import init_db, close_db
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.teardown_appcontext(close_db)
+app.register_blueprint(auth_bp)
 
 @app.cli.command("init-db")
 def init_db_command():
